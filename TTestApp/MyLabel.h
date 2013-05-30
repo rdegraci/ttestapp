8b5f0ba294ece0f0ddba8d00b603ec19fd70ca85
Rodney Degracia <rdegraci@elvenlabs.com>
2013-05-30 16:18:38 -0400

    Add MyLabel to Project

---
+++ TTestApp/MyLabel.h
@@ -0,0 +1,13 @@
+//
+//  MyLabel.h
+//  TTestApp
+//
+//  Created by Rodney Degracia on 5/30/13.
+//  Copyright (c) 2013 Organisation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLabel : UILabel
+
+@end
---
+++ TTestApp/MyLabel.m
@@ -0,0 +1,31 @@
+//
+//  MyLabel.m
+//  TTestApp
+//
+//  Created by Rodney Degracia on 5/30/13.
+//  Copyright (c) 2013 Organisation. All rights reserved.
+//
+
+#import "MyLabel.h"
+
+@implementation MyLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
